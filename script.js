26043adb12b8b1465d775b0b8930ee685972aa0d
ClaraBindheim <clara.bindheim@gmail.com>
2023-07-19 22:08:35 +0200

    Finale Abgabe

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 (function init(){
     "use strict";
 
-    //Um sicherzustellen das meine DOM-Elemente geladen sind, ansonsten ist animation nämlich null
-    window.onload = function(){
-
+    //Animation Vogel sagt Pieps wenn auf ihn geklickt wird
     const animation = document.getElementById("animation");
-    console.log(animation);
 
     function pieps(){
     
         if(animation.getElementsByTagName("p").length === 0){
 
-            var p = document.createElement("p");
-            var text = document.createTextNode("Pieps!");
+            const p = document.createElement("p");
+            const text = document.createTextNode("Pieps!");
             p.appendChild(text);
             animation.appendChild(p);
 
         }
         else{
-            var p = animation.getElementsByTagName("p");
+
+            const p = animation.getElementsByTagName("p");
             for(let i = 0; i < p.length; i++){
                 p[i].remove();
             }
@@ -27,8 +25,43 @@
     };
 
     animation.addEventListener("click", pieps);
-}
+
+
+    //Scroll-animation Bilder kommen ins Sichtfeld
+    const figures = document.querySelectorAll("figure");
+    console.log(figures);
+
+
+    const observer = new IntersectionObserver(entries => {
+
+        entries.forEach(entry => {
+            entry.target.classList.toggle("scroll", entry.isIntersecting);
+        })
+
+    },
+    {
+        rootMargin: "-20px",
+        threshold: 0,
+    }
+    )    
+
+    figures.forEach(figure => {
+        observer.observe(figure);
+    })
     
 
-})();
+    //Submit Form Nachricht anzeigen
+    const form = document.getElementsByClassName("m-form");
+    const submitNachricht = document.getElementById("submit");
+    
+    form[0].addEventListener('submit', (e) => {
+         e.preventDefault();
+        submitNachricht.classList.add('show');
+    });
+
+    form[0].addEventListener('reset', (e) => {
+       submitNachricht.classList.remove('show');
+   });
+
+})()
 
